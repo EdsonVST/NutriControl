91c80c6a92adfcaf86823901365ce7ae86a47179
Edson Vinicio <156148962+EdsonVST@users.noreply.github.com>
2026-07-20 14:14:10 -0300

    2.0.0.7
    ajustando tamanho da favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -91,8 +91,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       {
         rel: "stylesheet",
         href: appCss,
-      },
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      },      { rel: "shortcut icon", href: "/favicon.ico" },      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
       { rel: "apple-touch-icon", href: "/apple-touch-icon.png", sizes: "180x180" },
